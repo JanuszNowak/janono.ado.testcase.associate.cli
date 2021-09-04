5204a3291e47789ee2b13eb3080bedb32fcbe288
Janusz Nowak <janono@gmail.com>
2021-09-04 13:10:36 +0200

    dev branch init.

---
+++ src/janono.ado.testcase.associate.cli/Association.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace janono.ado.testcase.associate.cli
+{
+    internal class Association
+    {
+        public Association()
+        {
+        }
+
+        public string Organization { get; set; }
+        public string Assembly { get; set; }
+        public string Method { get; set; }
+        public int TestCaseId { get; set; }
+
+        public Guid AutomatedTestId { get; set; }
+        public bool NeedUpdateInsert { get; internal set; }
+        public string StatusCode { get; internal set; }
+    }
+}
