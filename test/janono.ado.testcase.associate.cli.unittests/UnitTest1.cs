914dfcf15578efaf7c90fb15b915f679b028f4f1
Janusz Nowak <janono@gmail.com>
2021-09-28 22:58:45 +0200

    Add unit test project.

---
+++ test/janono.ado.testcase.associate.cli.unittests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace janono.ado.testcase.associate.cli.unittests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
