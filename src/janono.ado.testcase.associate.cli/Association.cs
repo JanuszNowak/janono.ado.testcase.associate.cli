0b6bbed8712907ccfb9a0a9948acaafaa5416ad6
Janusz Nowak <janono@gmail.com>
2021-10-23 16:45:34 +0200

    update editorconfig

--- src/janono.ado.testcase.associate.cli/Association.cs
+++ src/janono.ado.testcase.associate.cli/Association.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace janono.ado.testcase.associate.cli
 {
--- test/janono.ado.testcase.associate.cli.unittests/UnitTest1.cs
+++ test/janono.ado.testcase.associate.cli.unittests/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace janono.ado.testcase.associate.cli.unittests
 {
