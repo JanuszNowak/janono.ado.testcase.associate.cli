f272b1fb967537dd3dd03b68b8844fb8d9d7c2c1
Janusz Nowak <janono@gmail.com>
2022-02-13 18:21:49 +0100

    Adding sample for xunit, fix assemly load by going to old version, remove not needed nuget for mstest framework.

---
+++ samples/sample.for_xunit_t_est.04/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace sample.for_xunit_t_est._04
+{
+    [janono.ado.testcase.associate.Organization("janono-pub")]
+    public class UnitTest1
+    {
+        [Fact]
+        [janono.ado.testcase.associate.TestCase(15)]
+        public void Test_XUnit_01()
+        {
+
+        }
+
+        [Fact]
+        [janono.ado.testcase.associate.TestCase(16)]
+        public void Test_XUnit_02()
+        {
+
+        }
+    }
+}
