9ea542e32f8d2d4bffc637a5a6826199b4587a7d
Janusz Nowak <janono@gmail.com>
2023-09-24 19:59:40 +0200

    Add test case net7 and net 8

---
+++ samples/sample.for_unit_t_est.05.net7/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace sample.for_unit_t_est._05.net7
+{
+    [TestClass]
+    [janono.ado.testcase.associate.Organization("janono-pub")]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [janono.ado.testcase.associate.TestCase(6)]
+        public void TestMethod1()
+        {
+        }        
+    }
+}
